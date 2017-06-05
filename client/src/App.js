ad15bff388e1c83903c59f8d495e4d4931b8cb19
Brigitte Huneke <brigittehuneke@gmail.com>
2017-06-05 16:29:24 -0700

    set up react router with links

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SignIn from './components/SignIn';
 import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-      </div>
+      <Router>
+        <div className="App">
+          < Route path='/' component={NavBar} />
+        </div>
+      </Router>
     );
   }
 }
