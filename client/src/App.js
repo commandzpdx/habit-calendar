66c92b6d85d90c1d8dbfdd8d24cd81b633a73764
Brigitte Huneke <brigittehuneke@gmail.com>
2017-06-05 15:24:08 -0700

    linked in NavBar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import SignIn from './components/SignIn';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SignIn />
+        <NavBar />
       </div>
     );
   }
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './NavBar.css';
+
+export default function NavBar() {
+  return (
+    <div className={styles.bar}>
+
+    </div>
+  )
+}
