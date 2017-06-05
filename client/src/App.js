f58890de4de660d4a6b5f5c1c43dbd18758efa20
Brigitte Huneke <brigittehuneke@gmail.com>
2017-06-05 14:46:34 -0700

    sass test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import SignIn from './components/SignIn';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <SignIn />
       </div>
     );
   }
---
+++ client/src/components/SignIn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './SignIn.css';
+
+export default function SignIn() {
+  return (
+    <div className={styles.body}> future signin form</div>
+  );
+}
+
