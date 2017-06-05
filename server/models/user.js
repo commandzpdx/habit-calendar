afc7eb894b6f1d29f3d61a7ac610b6c4b7cabaaf
cmd-kvn <kvn.m.wong@gmail.com>
2017-06-05 15:11:12 -0700

    creates user model

---
+++ server/models/user.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    set(password) { return bcrypt.hashSync(password, 8); },
+  },
+});
+
+schema.methods.comparePassword = function comparePassword(password) {
+  return bcrypt.compareSync(password, this.hash);
+};
+
+const User = mongoose.model('User', schema);
+module.exports = User;
