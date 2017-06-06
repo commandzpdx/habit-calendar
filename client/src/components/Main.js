e4b7238b872ec1c21f278433ec479ee2e0e5059a
Brigitte Huneke <brigittehuneke@gmail.com>
2017-06-05 17:00:31 -0700

    route for signin

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import SignIn from './SignIn';
 
 export default function Main() {
   return (
-    <div>
-      <SignIn />
-    </div>
-  )
+    <main>
+      <Route path="/signin" component={SignIn} />
+    </main>
+  );
 }
