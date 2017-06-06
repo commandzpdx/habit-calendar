af950dfb6999e675739e43256335a7349444f1bd
Brigitte Huneke <brigittehuneke@gmail.com>
2017-06-06 13:33:22 -0700

    blocked in homepage info

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import SignIn from './SignIn';
+import Home from './Home';
 
 export default function Main() {
   return (
     <main>
+      <Route exact path="/" component={Home} />
       <Route path="/signin" component={SignIn} />
     </main>
   );
