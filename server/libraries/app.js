cbcd969a9e44a0d4ed12f4d195dae585b56c618a
cmd-kvn <kvn.m.wong@gmail.com>
2017-06-06 14:44:35 -0700

    creates error handler implements error handler in app.js

--- server/libraries/app.js
+++ server/libraries/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 
 const users = require('../routes/Users');
+const errorHandler = require('../middlewares/handle-errors');
 
 const app = express();
 
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'));
 app.use('/api/users', users);
+
+app.use(errorHandler());
 
 module.exports = app;
