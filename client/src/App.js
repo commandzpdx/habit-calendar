09d93de42bbbb185eddc330cf34bf0e4a7c7e430
Brigitte Huneke <brigittehuneke@gmail.com>
2017-06-07 12:13:25 -0700

    linked up style sheet structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import SignIn from './components/SignIn';
+//import SignIn from './components/SignIn';
 import NavBar from './components/NavBar';
 import Main from './components/Main';
 import Footer from './components/Footer';
