50ff32bdfb4b558713d40fb994896943bd6e7aa2
Johnny Luangphasy <git@jluangphasy.com>
2017-06-07 13:56:07 -0700

    Promisify json web token library
    Create a promise for the sign and verify functions. Also, test the
    JWT library.

---
+++ server/libraries/json-web-token.js
@@ -0,0 +1,43 @@
+const jwt = require('jsonwebtoken');
+
+// Get secret key from env variable
+const { JWT_SECRET } = process.env;
+
+// JSON Web Token Library
+const jsonWebToken = {
+
+  /**
+   * Create a token.
+   *
+   * @param {Object} payload
+   * @return {Promise}
+   */
+  sign(payload) {
+    return new Promise((resolve, reject) => {
+      jwt.sign(payload, JWT_SECRET, null, (error, token) => (
+        error
+        ? reject(error)
+        : resolve(token)
+      ));
+    });
+  },
+
+  /**
+   * Verify a token.
+   *
+   * @param {String} token
+   * @return {Promise}
+   */
+  verify(token) {
+    return new Promise((resolve, reject) => {
+      jwt.verify(token, JWT_SECRET, { complete: true }, (error, payload) => (
+        error
+        ? reject(error)
+        : resolve(payload)
+      ));
+    });
+  },
+
+};
+
+module.exports = jsonWebToken;
