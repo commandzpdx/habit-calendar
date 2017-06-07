58f14a905bdb315f8633fd3989b3680414ef09c4
Johnny Luangphasy <git@jluangphasy.com>
2017-06-07 14:33:08 -0700

    Create and test ensure auth middleware

---
+++ server/test/middlewares/ensure-auth.test.js
@@ -0,0 +1,50 @@
+const { assert } = require('chai');
+const jwt = require('../../libraries/json-web-token');
+const ensureAuth = require('../../middlewares/ensure-auth')();
+
+describe.only('Ensure Auth Middleware', () => {
+
+  const payload = {
+    _id: '1a2b3c',
+  };
+
+  const req = {
+    get(header) {
+      return this.headers[header];
+    },
+  };
+
+  let token;
+
+  before(() => {
+    return jwt
+      .sign(payload)
+      .then((newToken) => {
+        token = newToken;
+      });
+  });
+
+  it('Should not set req.user property when token is invalid', () => {
+    req.headers = {
+      Authorization: 'Bearer invalid.token',
+    };
+
+    return ensureAuth(req, null, (err) => {
+      assert.notProperty(req, 'user');
+      assert.propertyVal(err, 'code', 401);
+      assert.propertyVal(err, 'message', 'Unauthorized');
+    });
+  });
+
+  it('Should set req.user property when token is valid', () => {
+    req.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+
+    return ensureAuth(req, null, () => {
+      assert.property(req, 'user');
+      assert.propertyVal(req.user, '_id', '1a2b3c');
+    });
+  });
+
+});
