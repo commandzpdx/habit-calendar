8251f3e097db94cd0ed6fa5bb9ab51e8e7651558
Brigitte Huneke <brigittehuneke@gmail.com>
2017-06-07 14:15:03 -0700

    set up main user page

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import SignIn from './SignIn';
 import Home from './Home';
+import UserMain from './UserMain';
 
 export default function Main() {
   return (
     <main>
       <Route exact path="/" component={Home} />
       <Route path="/signin" component={SignIn} />
+      <Route path="/firstname" component={UserMain} />
     </main>
   );
 }
+
+//TODO: replace /firstname with /:firstname when backend is connected
--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,16 +1,16 @@
 import React from 'react';
-//import styles from './SignIn.css';
+import styles from './SignIn.css';
 
 export default function SignIn() {
   return (
     <div>
       <h1>Sign in:</h1>
-      <form>
+      <form onSubmit={(e) => { e.preventDefault(); }}>
         <input type="text" placeholder="email" required />
         <br />
         <input type="text" placeholder="password" required />
         <br />
-        <button>submit</button>
+        <button type="submit">submit</button>
       </form>
     </div>
   );
