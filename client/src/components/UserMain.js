6289ae35723738d9d9a328629c32fc1763b501f2
Brigitte Huneke <brigittehuneke@gmail.com>
2017-06-08 14:57:28 -0700

    added and imported test spiral svg

--- client/src/components/UserMain.js
+++ client/src/components/UserMain.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import testSVG from '../assets/spiral_test.svg';
 
 export default function UserMain() {
   return (
@@ -6,7 +7,9 @@ export default function UserMain() {
       <h1>First name</h1>
       <h3>Category</h3>
       <h3>Habit (optional)</h3>
-      <div>calendar graphic</div>
+      <div>
+        <img src={testSVG}/>
+      </div>
     </div>
   );
 }
