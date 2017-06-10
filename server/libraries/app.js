831f28138c5059fcb41997afb77a85abb3b40e07
Johnny Luangphasy <contact@jluangphasy.com>
2017-06-09 19:27:37 -0700

    Move handle error middleware to api router

--- server/libraries/app.js
+++ server/libraries/app.js
@@ -2,12 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 
 const api = require('../routes/api');
-const errorHandler = require('../middlewares/handle-errors');
 
 const app = express();
 
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'));
 app.use('/api', api);
-app.use(errorHandler());
 
 module.exports = app;
