978c9faf9fa19298d405b96a0988136a5d5be6f4
Johnny Luangphasy <git@jluangphasy.com>
2017-06-09 20:42:53 -0700

    Create and test handle not found middleware

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,6 +5,7 @@ const authController = require('../controllers/auth');
 const userController = require('../controllers/user');
 const ensureAuth = require('../middlewares/ensure-auth');
 const handleErrors = require('../middlewares/handle-errors');
+const handleNotFound = require('../middlewares/handle-not-found');
 
 // Express router for API
 const apiRouter = Router();
@@ -17,6 +18,9 @@ apiRouter.get('/user/token', ensureAuth(), authController.token);
 
 // User signup
 apiRouter.post('/users', bodyParser(), userController.signup);
+
+// Handle not found (404) response
+apiRouter.use(handleNotFound());
 
 // Handle error response
 apiRouter.use(handleErrors());
