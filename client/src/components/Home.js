31e16f1e4de3c972602d46446efd5399e032d11c
cmd-kvn <kvn.m.wong@gmail.com>
2017-06-12 14:44:30 -0700

    creates SignUpForm component - replaces sign up jsx in Home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import SignUpForm from './SignUpForm';
+
 export default function Home() {
   return (
     <div>
@@ -8,20 +10,7 @@ export default function Home() {
 
       <h3>some sort of tagline</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce feugiat ultrices scelerisque. Proin consectetur, elit quis ultrices tempus, sem lorem finibus mi, nec finibus magna risus sit amet leo. Mauris a accumsan odio. Duis euismod sollicitudin nisi, in finibus mi mollis quis. Nam mattis urna sed neque sagittis, sit amet tempor tellus tincidunt. Sed maximus vitae massa dignissim imperdiet. Cras aliquam, urna non convallis euismod, quam velit dictum leo, at interdum arcu odio quis orci. Donec lorem ex, eleifend a molestie non, posuere et felis. Vivamus rhoncus erat ac mauris finibus, nec rhoncus turpis euismod. Donec id neque leo. Aenean eu rhoncus massa, at vehicula orci. Cras gravida mauris nisl, ornare eleifend nunc imperdiet at. Suspendisse potenti. Cras mi dui, condimentum eget diam quis, molestie commodo quam. Aenean in rhoncus lorem. Nunc cursus fermentum enim, non varius quam.</p>
-      <div>
-        <h3>Sign up:</h3>
-        <form>
-          <input type="text" placeholder="first name" required />
-          <br />
-          <input type="text" placeholder="last name" required />
-          <br />
-          <input type="text" placeholder="email" required />
-          <br />
-          <input type="text" placeholder="password" required />
-          <br />
-          <button>submit</button>
-        </form>
-      </div>
+      <SignUpForm />
     </div>
   );
 }
