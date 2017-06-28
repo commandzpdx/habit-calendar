d56dbef6d06b27182d1b6427d4da9cfde6501286
cmd-kvn <kvn.m.wong@gmail.com>
2017-06-28 16:37:32 -0700

    passes model test for monthCircle - example with all required fields

--- server/test/model.tests/habit-model.test.js
+++ server/test/model.tests/habit-model.test.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert;
 
 const Habit = require('../../models/habit');
 
-describe.only('habit model', () => {
+describe('habit model', () => {
   it('example with all required fields', () => {
     return new Habit({
       category: 'asdf',
