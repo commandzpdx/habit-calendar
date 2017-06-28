d8f38d05ba36b6f8234a1e8ed51d083fe24ba1b9
cmd-kvn <kvn.m.wong@gmail.com>
2017-06-28 15:31:53 -0700

    passes tests for dayCircle model - when one required field (pathD,  textContent, textTransform) is missing in the schema

--- server/test/model.tests/dayCircle-model.test.js
+++ server/test/model.tests/dayCircle-model.test.js
@@ -2,14 +2,48 @@ const assert = require('chai').assert;
 
 const DayCircle = require('../../models/dayCircle');
 
-describe.only('dayCircle model', () => {
+describe('dayCircle model', () => {
   it('example with all required fields', () => {
     return new DayCircle({
-      pathD: 'thePath',
-      textContent: 1234,
-      textTransform: 'some text',
+      pathD: 'abc',
+      textContent: 123,
+      textTransform: 'asdf',
     }).validate();
   });
 
-  
+  it('requires pathD (invalid when no pathD)', () => {
+    const dayCircle1 = new DayCircle({
+      textContent: 123,
+      textTransform: 'asdf',
+    });
+    return dayCircle1.validate()
+    .then(
+      () => { throw new Error('validation should not pass'); },
+      err => assert.isNotNull(err),
+    );
+  });
+
+  it('requires textContent (invalid when no textContent', () => {
+    const dayCircle2 = new DayCircle({
+      pathD: 'abc',
+      textTransform: 'asdf',
+    });
+    return dayCircle2.validate()
+    .then(
+      () => { throw new Error('validation should not pass'); },
+      err => assert.isNotNull(err),
+    );
+  });
+
+  it('requires textTransform (invalid when no textTransform', () => {
+    const dayCircle3 = new DayCircle({
+      pathD: 'abc',
+      textContent: 123,
+    });
+    return dayCircle3.validate()
+    .then(
+      () => { throw new Error('validation should not pass'); },
+      err => assert.isNotNull(err),
+    );
+  });
 });
