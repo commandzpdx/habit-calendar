7e80ccd5d7fd0e61e0705492660a77ca5d0d536b
cmd-kvn <kvn.m.wong@gmail.com>
2017-06-28 15:06:36 -0700

    passes tests for day model - with all required fields - with missing required fields

--- server/test/model.tests/day-model.test.js
+++ server/test/model.tests/day-model.test.js
@@ -2,20 +2,30 @@ const assert = require('chai').assert;
 
 const Day = require('../../models/day');
 
-let testDay = {
-  circleFilled: true,
-  year: 2001,
-  weekday: 'Monday',
-  dayCircle: {},
-  habit: {},
-};
-
-describe.only('day model', () => {
-  it('is valid with circleFilled and year', () => {
+describe('day model', () => {
+  it('example with all required fields', () => {
     return new Day({
       circleFilled: true,
       year: 1999,
     }).validate();
+  });
+
+  it('requires circleFilled (validation fails when no circleFilled)', () => {
+    const day1 = new Day({ year: 2001 });
+    return day1.validate()
+      .then(
+      () => { throw new Error('validation should not pass'); },
+      err => assert.isNotNull(err),
+    );
+  });
+
+  it('requires year (validation fails when no year)', () => {
+    const day2 = new Day({ circleFilled: false });
+    return day2.validate()
+      .then(
+      () => { throw new Error('validation should not pass'); },
+      err => assert.isNotNull(err),
+    );
   });
 
 });
