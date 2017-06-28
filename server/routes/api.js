3370ec6296cbe4dfed4ebb7d53db6431dd63ad57
Brigitte Huneke <brigittehuneke@gmail.com>
2017-06-28 14:45:00 -0700

    basic CRUD for monthCircle

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,6 +3,7 @@ const { json: bodyParser } = require('body-parser');
 
 const authController = require('../controllers/auth');
 const userController = require('../controllers/user');
+const monthCircleController = require('../controllers/monthCircle');
 const ensureAuth = require('../middlewares/ensure-auth');
 const handleErrors = require('../middlewares/handle-errors');
 const handleNotFound = require('../middlewares/handle-not-found');
@@ -18,6 +19,17 @@ apiRouter.get('/user/token', ensureAuth(), authController.token);
 
 // User signup
 apiRouter.post('/users', bodyParser(), userController.signup);
+
+// MonthCircle CRUD
+apiRouter.post('/month-circles', bodyParser(), monthCircleController.createMonth);
+
+apiRouter.get('/month-circles', monthCircleController.getMonths);
+
+apiRouter.get('/month-circles/:id', monthCircleController.getMonth);
+
+apiRouter.delete('/month-circles/:id', monthCircleController.deleteMonth);
+
+apiRouter.put('/month-circles/:id', bodyParser(), monthCircleController.updateMonth);
 
 // Handle not found (404) response
 apiRouter.use(handleNotFound());
