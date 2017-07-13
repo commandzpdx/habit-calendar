313374a6d56d0c836edb1f709cf7293b4ce738be
Johnny Luangphasy <git@jluangphasy.com>
2017-07-13 00:01:35 -0700

    Populate dayCircles within monthCircles
    Use aggregation to create a dayCircles property on each month
    circle object.

--- server/controllers/circle.js
+++ server/controllers/circle.js
@@ -1,25 +1,10 @@
-const DayCircle = require('../models/dayCircle');
 const MonthCircle = require('../models/monthCircle');
 
 const circleController = {
   getCircles(req, res, next) {
-    let circles = [];
-    MonthCircle.find()
-      .then((months) => {
-        circles = months;
-        return Promise.all(months.map((month) => {
-          return DayCircle.find({ month: month._id });
-        }));
-      })
-      .then((days) => {
-        // return circles.map((month, idx) => {
-        //   month.dayCircles = "days[idx]";
-        //   return month;
-        // });
-      })
-      // .then((newCircles) => {
-      //   res.json(newCircles);
-      // })
+    return MonthCircle
+      .findMonthCircles()
+      .then(circles => res.json(circles))
       .catch(next);
   },
 };
