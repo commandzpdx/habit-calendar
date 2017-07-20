9a33e1e053108b8e0d12e8975624945af40eaa71
cmd-kvn <kvn.m.wong@gmail.com>
2017-07-20 14:26:18 -0700

    - changes state in App to include the habitCategory and habit - changes UserSetUp component into a class

--- client/src/components/UserSetUp.js
+++ client/src/components/UserSetUp.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function UserSetUp() {
-  return (
-    <div>
-      <h1>SetUp</h1>
-      <form>
-        <select>
-          <option>Exercise</option>
-          <option>Diet</option>
-          <option>Habit</option>
-          <option>Chore</option>
-        </select>
-        <br />
-        <input type="text" placeholder="habit" />
-        <br />
-        <button type="submit">submit</button>
-      </form>
-    </div>
-  );
+class UserSetUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      habitCategory: '',
+      habit: '',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>SetUp</h1>
+        <form>
+          <select id="habit">
+            <option value={0}>Exercise</option>
+            <option value={1}>Diet</option>
+            <option value={2}>Habit</option>
+            <option value={3}>Chore</option>
+          </select>
+          <br />
+          <input type="text" placeholder="habit" />
+          <br />
+          <button type="submit">submit</button>
+        </form>
+      </div>
+    );
+  }
 }
+
+UserSetUp.propTypes = {
+
+};
+
+export default UserSetUp;
