3d473ec033e13143a3f752788bf5eac9e76b238e
cmd-kvn <kvn.m.wong@gmail.com>
2017-07-25 16:04:07 -0700

    passes props name, habit, habtiCategory to Main - Main continues to pass to UserMain - UserMain displays message to user's first name about their habitCategory or habit + habitCategory

--- client/src/components/UserMain.js
+++ client/src/components/UserMain.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import SpiralCal from './SpiralCal';
 
-export default function UserMain() {
+export default function UserMain(props) {
+  const nameArray = props.name.split(' ');
+  const firstName = nameArray[0];
+
   return (
     <div>
-      <h1>First name</h1>
-      <h3>Category</h3>
-      <h3>Habit (optional)</h3>
+      <h1>Hi {firstName}</h1>
+      <h3>didju do your {props.habit
+        ? <h3>{props.habit} {props.habitCategory} today?</h3>
+        : <h3>{props.habitCategory} today?</h3>}
+      </h3>
       <div>
         <SpiralCal />
       </div>
     </div>
   );
 }
+
+UserMain.propTypes = {
+  name: PropTypes.string.isRequired,
+  habit: PropTypes.string.isRequired,
+  habitCategory: PropTypes.string.isRequired,
+};
