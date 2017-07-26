bbb5e1c6f607a5585b588bad9614ed7327de6111
Johnny Luangphasy <git@jluangphasy.com>
2017-07-26 15:19:29 -0700

    Create promise for server start and stop functions

--- server/libraries/server.js
+++ server/libraries/server.js
@@ -3,20 +3,30 @@ const http = require('http');
 const app = require('./app');
 const { PORT } = require('../config/constants');
 
-const httpServer = http.createServer(app);
+// Create HTTP server.
+const server = http.createServer(app);
 
-httpServer.on('listening', () => {
-  console.log(`server is running at port ${PORT}`);
+// Log message when server starts.
+server.on('listening', () => {
+  console.log(`server running at port ${PORT}`);
 });
 
-httpServer.on('close', () => {
-  console.log(`server stopped running at port ${PORT}`);
+// Log message when server stops.
+server.on('close', () => {
+  console.log(`server stopped at port ${PORT}`);
 });
 
-const server = {
-  start(port = PORT) {
-    return httpServer.listen(port);
-  },
+// Start HTTP server.
+const start = (port = PORT) => new Promise((resolve, reject) => {
+  server.listen(port, err => (err ? reject(err) : resolve()));
+});
+
+// Stop HTTP server.
+const stop = () => new Promise((resolve, reject) => {
+  server.close(err => (err ? reject(err) : resolve()));
+});
+
+module.exports = {
+  start,
+  stop,
 };
-
-module.exports = server;
