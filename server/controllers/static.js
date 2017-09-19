f696dd38c1b781f02b5796d2b8ca6b91f46fd23a
Johnny Luangphasy <git@jluangphasy.com>
2017-09-19 09:35:03 -0700

    Uppercase all characters for constants
    Update all of the constants in the server-side files.

--- server/controllers/static.js
+++ server/controllers/static.js
@@ -1,8 +1,8 @@
-const { indexHtml } = require('../config/paths');
+const PATHS = require('../constants/paths');
 
 const staticController = {
   index(req, res) {
-    return res.sendFile(indexHtml);
+    return res.sendFile(PATHS.SERVER_INDEX);
   },
 };
 
--- server/libraries/server.js
+++ server/libraries/server.js
@@ -1,24 +1,24 @@
 const http = require('http');
 
 const app = require('./app');
-const { PORT } = require('../config/constants');
+const ENV = require('../constants/env');
 
 // Create HTTP server.
 const server = http.createServer(app);
 
 // Log message when server starts.
 server.on('listening', () => {
-  console.log(`server running at port ${PORT}`);
+  console.log(`server running at port ${ENV.PORT}`);
 });
 
 // Log message when server stops.
 server.on('close', () => {
-  console.log(`server stopped at port ${PORT}`);
+  console.log(`server stopped at port ${ENV.PORT}`);
 });
 
 // Start HTTP server.
-const start = (port = PORT) => new Promise((resolve, reject) => {
-  server.listen(port, err => (
+const start = (port = ENV.PORT, host = ENV.HOST) => new Promise((resolve, reject) => {
+  server.listen(port, host, err => (
     err
     ? reject(err)
     : resolve()
