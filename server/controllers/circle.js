21acd630d7441441d3169041cd637fc29b9d0f12
Johnny Luangphasy <git@jluangphasy.com>
2017-09-19 10:03:44 -0700

    Cleanup controllers

--- server/controllers/circle.js
+++ server/controllers/circle.js
@@ -1,12 +1,16 @@
+/**
+ * Circle Controller.
+ *
+ * @module server/controllers/circle
+ */
+
 const DayCircle = require('../models/dayCircle');
 
-const circleController = {
-  getCircles(req, res, next) {
-    return DayCircle
-      .findDays()
-      .then(circles => res.json(circles))
-      .catch(next);
-  },
-};
+const getCircles = (req, res, next) => DayCircle
+  .findDays()
+  .then(circles => res.json(circles))
+  .catch(next);
 
-module.exports = circleController;
+module.exports = {
+  getCircles,
+};
--- server/controllers/monthCircle.js
+++ server/controllers/monthCircle.js
@@ -1,52 +1,44 @@
+/**
+ * Month Circle Controller.
+ *
+ * @module server/controllers/monthCircle
+ */
+
 const MonthCircle = require('../models/monthCircle');
 
-const monthCircleController = {
-  createMonth(req, res, next) {
-    const data = req.body;
+const createMonth = (req, res, next) => {
+  const data = req.body;
 
-    return new MonthCircle(data).save()
-      .then((month) => {
-        return res.json(month);
-      })
-      .catch(next);
-  },
-
-  getMonths(req, res, next) {
-
-    return MonthCircle.find()
-    .then((months) => {
-      return res.json(months);
-    })
+  return new MonthCircle(data)
+    .save()
+    .then(month => res.json(month))
     .catch(next);
-  },
-
-  getMonth(req, res, next) {
-
-    return MonthCircle.find(req.params.id)
-    .then((month) => {
-      return res.json(month);
-    })
-    .catch(next);
-  },
-
-  deleteMonth(req, res, next) {
-
-    return MonthCircle.findByIdAndRemove(req.params.id)
-    .then(() => {
-      return res.json({ message: 'Month was deleted' });
-    })
-    .catch(next);
-  },
-
-  updateMonth(req, res, next) {
-
-    return MonthCircle.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then((updatedMonth) => {
-      return res.json(updatedMonth);
-    })
-    .catch(next);
-  },
-
 };
 
-module.exports = monthCircleController;
+const getMonths = (req, res, next) => MonthCircle
+  .find()
+  .then(months => res.json(months))
+  .catch(next);
+
+const getMonth = (req, res, next) => MonthCircle
+  .find(req.params.id)
+  .then(month => res.json(month))
+  .catch(next);
+
+const deleteMonth = (req, res, next) => MonthCircle
+  .findByIdAndRemove(req.params.id)
+  .then(() => res.json({ message: 'Month was deleted' }))
+  .catch(next);
+
+const updateMonth = (req, res, next) => MonthCircle
+  .findByIdAndUpdate(req.params.id, req.body, { new: true })
+  .then(updatedMonth => res.json(updatedMonth))
+  .catch(next);
+
+module.exports = {
+  createMonth,
+  deleteMonth,
+  getMonth,
+  getMonths,
+  updateMonth,
+};
