b760353d8bfb461b4951cc60c74a8bdec22194cc
Johnny Luangphasy <git@jluangphasy.com>
2017-09-21 14:58:56 -0700

    Refactor ensureAuth function

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -8,19 +8,18 @@ const jwt = require('../libraries/jsonWebToken');
 
 // Check if JSON web token is valid or not.
 const ensureAuth = () => (req, res, next) => {
-  const authHeader = req.get('Authorization');
+  const [bearer, token] = req
+    .get('Authorization')
+    .split(' ');
   const unauthorized = {
     code: 401,
     message: 'Unauthorized',
   };
 
   // No Authorization header or "Bearer" isn't in the header
-  if (!authHeader || authHeader.substr(0, 6) !== 'Bearer') {
+  if (bearer !== 'Bearer' || !token) {
     return next(unauthorized);
   }
-
-  // Token starts after the word Bearer and a space
-  const token = authHeader.slice(7);
 
   return jwt
     .verify(token)
