869d9264ebe82fb2a9b020fa9569def1bb0efb3b
Johnny Luangphasy <contact@jluangphasy.com>
2017-09-21 15:07:15 -0700

    Use Error constructor for unauth error

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -8,17 +8,15 @@ const jwt = require('../libraries/jsonWebToken');
 
 // Check if JSON web token is valid or not.
 const ensureAuth = () => (req, res, next) => {
-  const [bearer, token] = req
-    .get('Authorization')
-    .split(' ');
-  const unauthorized = {
-    code: 401,
-    message: 'Unauthorized',
-  };
+  const authorization = req.get('Authorization') || '';
+  const [bearer, token] = authorization.split(' ');
+
+  const newError = new Error('Unauthorized');
+  newError.code = 401;
 
   // No Authorization header or "Bearer" isn't in the header
   if (bearer !== 'Bearer' || !token) {
-    return next(unauthorized);
+    throw newError;
   }
 
   return jwt
@@ -31,7 +29,7 @@ const ensureAuth = () => (req, res, next) => {
     })
     .catch(() => {
       // Token is invalid, set response to 401
-      next(unauthorized);
+      throw newError;
     });
 };
 
