277b35cc0e603c4dd6f5ff2408afa0fa45e8873b
Johnny Luangphasy <git@jluangphasy.com>
2017-09-26 16:38:50 -0700

    Update client dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './libraries/registerServiceWorker';
 import './index.css';
+import App from './components/App';
+import registerServiceWorker from './libraries/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
